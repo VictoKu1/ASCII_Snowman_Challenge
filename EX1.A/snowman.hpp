393a3110bf44a2628e82fbeffae1738ec9cf3dce
Victor <kushnir.victor@gmail.com>
2021-03-13 00:11:47 +0200

    .

---
+++ EX1.A/snowman.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648 
+#include <string>
+#include <stdexcept>
+#include <stdio.h> 
+#include <string.h> 
+namespace ariel{
+    std::string snowman(int dna);
+}
+bool isBase3(int num);
+//*Following code were taken and editted from https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/ to convert base 10 number to a given base.
+char reVal(int num) ; 
+void strev(char *str);
+int fromDeci(char res[], int base, int inputNum);
+
